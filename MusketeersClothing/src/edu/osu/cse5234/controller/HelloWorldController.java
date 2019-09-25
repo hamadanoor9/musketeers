0714c91dcf0ef69c0a11878ada86933a1b732ce9
meph13topheles <meph13topheles@172.28.37.184>
2019-09-25 19:29:23 -0400

    Step 5. Not completed yet but I'll finish this step soon.

--- MusketeersClothing/src/edu/osu/cse5234/controller/HelloWorldController.java
+++ MusketeersClothing/src/edu/osu/cse5234/controller/HelloWorldController.java
@@ -2,7 +2,6 @@ package edu.osu.cse5234.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +18,6 @@ public class HelloWorldController {
 	@RequestMapping(path = "/new", method = RequestMethod.GET)
 	public String printHelloNew() throws Exception {
 		return "HelloJSP";
-		
 	}
 
 
