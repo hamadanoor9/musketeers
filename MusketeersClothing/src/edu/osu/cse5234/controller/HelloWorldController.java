ad6a10e39cfd365855b2cd1f110076a6518b940e
honghuibao <honghuibao@172.28.139.201>
2019-10-07 18:54:54 -0400

    Change something

--- MusketeersClothing/src/edu/osu/cse5234/controller/HelloWorldController.java
+++ MusketeersClothing/src/edu/osu/cse5234/controller/HelloWorldController.java
@@ -9,21 +9,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloWorldController {
-	
+
 	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public String printHelloNew() throws Exception {
 		return "Home";
 	}
-	
+
 	@RequestMapping(path = "/aboutus", method = RequestMethod.GET)
 	public String aboutUs() throws Exception {
 		return "AboutUs";
 	}
-	
+
 	@RequestMapping(path = "/contactus", method = RequestMethod.GET)
 	public String contactUs() throws Exception {
 		return "ContactUs";
 	}
-	
 
 }
