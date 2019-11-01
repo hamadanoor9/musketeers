6c8fd4bdae8c7b2b03e679a20205a339defebe90
baohonghui <baohonghui1996@gmail.com>
2019-10-31 21:41:14 -0400

    Lab 8 update

--- MusketeersClothing/src/edu/osu/cse5234/model/LineItem.java
+++ MusketeersClothing/src/edu/osu/cse5234/model/LineItem.java
@@ -1,10 +1,27 @@
 package edu.osu.cse5234.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="CUSTOMER_ORDER_LINE_ITEM")
 public class LineItem {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
 	private int id;
+	@Column(name="ITEM_NUMBER")
 	private int itemNumber;
+	@Column(name="ITEM_NAME")
 	private String itemName;
+	@Transient
 	private double price;
+	@Column(name="QUANTITY")
 	private int quantity;
 	
 	public int getId() {
